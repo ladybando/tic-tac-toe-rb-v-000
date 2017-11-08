d12268bb031fcff7a36a89f3d640ba582f896ca7
Ayana Bando <ayanaflax@yahoo.com>
2017-11-08 14:16:13 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -67,23 +67,12 @@ def position_taken?(board, index)
   board[index] == "X" || board[index] == "O"
 end
 
-def turn_count(board)
-  counter = 0
-  board.each do |move|
-    if move == "X" || move == "O"
-      counter += 1
-    end
-  end
-  return counter
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
 end
 
 def current_player(board)
-  turn_count(board)
-  if  turn_count(board) % 2 == 0
-    return "X"
-  else
-    return "O"
-  end
+  turn_count(board) % 2 == 0 ? "X" : "O"
 end
 
 def over?(board)
