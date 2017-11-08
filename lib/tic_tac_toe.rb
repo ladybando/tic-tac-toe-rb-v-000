275c4f6d11c303a4fe93c3e796a3192f5eb58fe4
Ayana Bando <ayanaflax@yahoo.com>
2017-11-08 14:28:23 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -59,10 +59,6 @@ def turn(board)
   end
 end
 
-def move(board, index, char = "X")
-  board[index] = char
-end
-
 def position_taken?(board, index)
   board[index] == "X" || board[index] == "O"
 end
@@ -84,3 +80,4 @@ def winner(board)
     board[winning_combo.first]
   end
 end
+                
