0f9057d4bc87ae54f318b7ca1c8d2608b58cd43a
Ayana Bando <ayanaflax@yahoo.com>
2017-11-08 14:32:05 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,7 +1,12 @@
-WIN_COMBINATIONS= [
-  [0,1,2], [3,4,5], [6,7,8],
-  [2,4,6], [0,4,8],
-  [0,3,6], [1,4,7], [2,5,8]
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2]
 ]
 
 def play(board)
@@ -47,14 +52,14 @@ def over?(board)
   won?(board) || draw?(board)
 end
 
-def input_to_index(player_input)
-  player_input.to_i - 1
+def input_to_index(user_input)
+  user_input.to_i - 1
 end
 
 def turn(board)
   puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(player_input)
+  user_input = gets.strip
+  index = input_to_index(user_input)
   if valid_move?(board, index)
     move(board, index, current_player(board))
     display_board(board)
@@ -64,8 +69,27 @@ def turn(board)
 end
 
 def position_taken?(board, index)
-  board[index] == "X" || board[index] == "O"
+  board[index]== "X" || board[index] == "O"
 end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
+
+def turn_count(board)
+  board.count{|token| token == "X" || token == "O"}
+end
+
+def move(board, index, player)
+  board[index] = player
+end
+
+def winner(board)
+  if winning_combo = won?(board)
+    board[winning_combo.first]
+  end
+end
+
 
 def current_player(board)
   turn_count(board) % 2 == 0 ? "X" : "O"
