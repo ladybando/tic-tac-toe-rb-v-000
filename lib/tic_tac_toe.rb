304b2fb7d650e04eba6deea5fa926365073fc726
Ayana Bando <ayanaflax@yahoo.com>
2017-11-08 14:04:55 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,6 +15,10 @@ def play(board)
   end
 end
 
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -33,10 +37,6 @@ end
 
 def position_taken?(board, index)
   board[index] != " "
-end
-
-def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
 end
 
 def turn(board)
